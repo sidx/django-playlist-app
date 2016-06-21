6e7c2759bb64591ff0586372e848f8bc70641579
sidx <sidharth.xtb@gmail.com>
2016-06-21 21:28:24 +0530

    added track,genre list, detail views. ToDo - Edit and add new items

--- songs/models.py
+++ songs/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 
 class songModel(models.Model):
+	id = models.AutoField(primary_key=True)
 	title = models.CharField(max_length=200)
 	rating = models.IntegerField()
 	
@@ -12,8 +13,9 @@ class songModel(models.Model):
 		return self.title
 		
 class genreModel(models.Model):
+	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=30)
-	songs = models.ManyToManyField(songModel)
+	songs = models.ManyToManyField(songModel,related_name='genres')
 	
 	def __unicode__(self):
 		return self.name
--- songs/views.py
+++ songs/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import songModel, genreModel
 
 # Create your views here.
 
 def tracks_list(request):
-	return render(request, 'songs/tracks_list.html')
+	allTracks = songModel.objects.filter()
+	return render(request, 'songs/tracks_list.html',{'allTracks':allTracks})
 	
+def genre_list(request):
+	allGenres = genreModel.objects.filter()
+	return render(request, 'songs/genres_list.html',{'allGenres':allGenres})
+	
+def track_detail(request,id):
+	print id
+	track = songModel.objects.get(id=id)
+	return render(request, 'songs/track_detail.html',{'track':track})
+
+def genre_detail(request,id):
+	print id
+	genre = genreModel.objects.get(id=id)
+	return render(request, 'songs/genre_detail.html',{'genre':genre})
